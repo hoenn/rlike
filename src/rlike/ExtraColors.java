69df301cf09088c66259f5428ec1ed9d17b65bc0
Hoenn <skatevan1313@gmail.com>
2016-01-29 22:52:37 -0500

    Added more colors

--- src/rlike/ExtraColors.java
+++ src/rlike/ExtraColors.java
@@ -4,4 +4,7 @@ import java.awt.Color;
 
 public class ExtraColors {
 	public static Color darkBrown = new Color(102, 51, 0);
+	public static Color slateGray = new Color(112, 138, 144);
+	public static Color olive = new Color(85, 107, 47);
+	public static Color rosyBrown = new Color(188, 143, 143);
 }
