a94e06bac4caff89af430427cf253c0826f22178
Hoenn <skatevan1313@gmail.com>
2016-01-30 21:36:05 -0500

    Added additional colors to ExtraColors

--- src/rlike/ExtraColors.java
+++ src/rlike/ExtraColors.java
@@ -7,4 +7,8 @@ public class ExtraColors {
 	public static Color slateGray = new Color(112, 138, 144);
 	public static Color olive = new Color(85, 107, 47);
 	public static Color rosyBrown = new Color(188, 143, 143);
+	public static Color mediumOrchid = new Color(186, 85, 211);
+	public static Color lightCoral = new Color(240, 128, 128);
+	public static Color wheat = new Color(245, 222, 179);
+	public static Color mediumPurple = new Color(147, 112, 219);
 }
