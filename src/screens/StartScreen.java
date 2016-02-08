43b41633f044e2f1ba3870316b208671c2bf7db2
Hoenn <skatevan1313@gmail.com>
2016-02-07 19:43:19 -0500

    Added a few symbols to choose from for background in start screen

--- src/screens/StartScreen.java
+++ src/screens/StartScreen.java
@@ -8,16 +8,16 @@ import asciiPanel.AsciiPanel;
 import rlike.ExtraColors;
 
 public class StartScreen implements Screen {
-
+	private char bg;
 	@Override
 	public void displayOutput(AsciiPanel terminal) {
 		//Fill background
+		pickRandomBg();	
 		for(int r = 0; r<24; r++) {
 			for(int c =0; c<80; c++) {
-				terminal.write('.', c, r, AsciiPanel.brightBlack);
+				terminal.write(bg, c, r, AsciiPanel.brightBlack);
 			}
-		}
-		
+		}	
 		
 		//Draw Image
 		ArrayList<String> img = new ArrayList<String>();
@@ -37,8 +37,9 @@ public class StartScreen implements Screen {
 		img.add("  .@@@@@@@       @@@@@@@.  ");
 		img.add("   ......         ......   ");
 		randomizeCharacter(img);
-		int xx = (int)(Math.random()*50)+1;
-		int yy = (int) (Math.random()*10);
+		
+		int xx = (int)(Math.random()*(80-img.get(0).length()));
+		int yy = (int) (Math.random()*(24-img.size()));
 		for(int i =0; i<img.size(); i++) {
 			terminal.write(img.get(i), xx, i+yy, ExtraColors.mediumOrchid);
 		}
@@ -50,6 +51,16 @@ public class StartScreen implements Screen {
 		terminal.write("Quest!", 47, 10);
 		terminal.writeCenter("Press Enter to Begin", 22);
 		
+	}
+	public void pickRandomBg() {
+		int rand = (int)(Math.random()*4);
+		bg = ' ';
+		switch(rand) {
+			case 0: bg = '`'; break;
+			case 1: bg = '.'; break;
+			case 2: bg = ','; break;
+			case 3: bg = (char)59; break;
+		}
 	}
 	public void randomizeCharacter(List<String> list) {
 		String randomSymbol = Character.toString((char)((int)(Math.random()*255)+1));
